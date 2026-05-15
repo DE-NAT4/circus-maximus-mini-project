e8499150bb8a2ca5ad6af09fa9a344a50f5f5db5
Kaleb-Mat <1326762@genstudents.org>
2026-05-15 11:34:03 +0100

    update database

--- database_test.py
+++ database_test.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_connection():
     return psycopg2.connect(
         host =os.getenv('postgres_host'),
@@ -17,16 +18,46 @@ def create_table():
     conn = get_connection() # call the get connection function to establish a connection
     cur = conn.cursor() # call the cursor method aginst the 'conn' object
 
+
     cur.execute('''
-               create table if not exists more_products (
+               create table if not exists products (
                id serial primary key,
                name text not null,
                price numeric(10,2) not null
                )
-               ''') # execute the sql statement to create a table;
+               ''') # execute the sql statement to create the products table;
+    cur.execute('''
+                create table if not exists couriers (
+                id serial primary key,
+                name text not null,
+                phone_number integer not null
+                )
+                ''') # execute the sql statement to create the couriers table;
+    cur.execute('''
+                create table if not exists orders (
+                id serial primary key,
+                customer_name text not null,
+                customer_address text not null,
+                customer_phone integer not null,
+                courier_id integer references couriers(id),
+                status text not null,
+                items integer not null references products(id)
+                )
+                ''') # execute the squl statement to create the orders table;  
+
+    with open('Orders.csv', 'r') as f:
+      next(f)  # Skip header
+      cur.copy_from(f, 'orders', sep=',', null='', 
+                    columns=('customer_name', 
+                             'customer_address',
+                               'customer_phone',
+                                 'courier_id',
+                                   'status',
+                                     'items'))
     conn.commit() # commit chages to DB
     cur.close() # close cursor
     conn.close() # close connection
     print("Table created successfully")
 
 create_table()
+
